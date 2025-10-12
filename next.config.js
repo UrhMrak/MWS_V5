dc6274e53193ad467c4079cf22c45b6510676c93
Urh Mrak <41495374+UrhMrak@users.noreply.github.com>
2025-10-12 22:29:20 +0000

    Fix base path configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? "/MWS_V5" : "";
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  basePath: basePath,
+  assetPrefix: basePath,
 };
 
 module.exports = nextConfig;
